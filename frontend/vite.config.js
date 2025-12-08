51110ccf5828629cc9635fa54fee8a17b06b1171
Fauzan <fauzanahsanudin@gmail.com>
2025-12-08 14:22:59 +0700

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [svgr(), react()],
-  base: '/melodia_apps/', 
-})
+  base: '/melodia_apps/',
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg', // optional, biar semua .svg bisa diimport sebagai component
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // dalam kB
+  },
+});
